503ce70fe04e61a5a7b4534d77774a6744cc1ac1
jujulioed <julioeduardon9@hotmail.com>
2026-02-08 16:54:18 -0300

    Implement tests
    Signed-off-by: jujulioed <julioeduardon9@hotmail.com>

---
+++ src/test/java/com/jujulioed/simpleschedule/tasks/ScheduleTaskTestUnit.java
@@ -0,0 +1,17 @@
+package com.jujulioed.simpleschedule.tasks;
+
+import org.junit.Before;
+
+import org.junit.Test;
+import org.mockito.InjectMocks;
+
+public class ScheduleTaskTestUnit {
+
+    @InjectMocks
+    private ScheduledTasks tasks;
+
+    @Test
+    public void workSucess() {
+
+    }
+}
--- src/test/java/com/jujulioed/simpleschedule/tasks/ScheduledTasksTest.java
+++ src/test/java/com/jujulioed/simpleschedule/tasks/ScheduledTasksTest.java
@@ -2,17 +2,18 @@ package com.jujulioed.simpleschedule.tasks;
 
 import org.awaitility.Durations;
 import org.junit.jupiter.api.Test;
-import org.mockito.Spy;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 
-import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 import static org.awaitility.Awaitility.await;
 import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.verify;
 
 @SpringBootTest
+@TestPropertySource(properties = "task.work.rate=100")
 public class ScheduledTasksTest {
 
     @MockitoSpyBean
@@ -20,8 +21,13 @@ public class ScheduledTasksTest {
 
     @Test
     public void reportCurrentTime() {
-        await().atMost(Durations.TEN_SECONDS).untilAsserted(() -> {
+        await().atMost(10, TimeUnit.SECONDS).untilAsserted(() -> {
             verify(tasks, atLeast(2)).reportCurrentTime();
         });
+    }
+
+    @Test
+    public void testFunctionCurrentTime() {
+
     }
 }
